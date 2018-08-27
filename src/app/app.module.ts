b48b54e9172ee678174ae6c45956d104e977d93a
Yomar Miranda <mirandayomar@gmail.com>
2018-08-27 00:29:57 -0500

    Creating the chat window.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { environment } from '../environments/environment';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { ChatComponent } from './components/chat/chat.component';
 
@@ -19,7 +21,8 @@ import { ChatComponent } from './components/chat/chat.component';
     AngularFireModule.initializeApp(environment.firebase), // imports firebase/app needed for everything
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-    AngularFireStorageModule // imports firebase/storage only needed for storage features
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styles: []
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent {
+  message = '';
 
   constructor() { }
 
-  ngOnInit() {
+  sendMessage() {
+    console.log(this.message);
   }
 
 }
