77f63d43708e5aa00a425ad0ff1777654e0a08f6
Yomar Miranda <mirandayomar@gmail.com>
2018-08-30 18:20:35 -0500

    Logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
+import { ChatService } from './providers/chat.service';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +7,6 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  chats: Observable<any[]>;
 
-  constructor(db: AngularFirestore) {
-    this.chats = db.collection('chats').valueChanges();
-  }
+  constructor(public _chat: ChatService) { }
 }
--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { auth } from 'firebase';
 
 import { Message } from '../interfaces/message.interface';
 
@@ -11,8 +13,32 @@ import { Message } from '../interfaces/message.interface';
 export class ChatService {
   private itemsCollection: AngularFirestoreCollection<Message>;
   public chats: Message[] = [];
+  public user: any = {};
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore, public afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe(user => {
+      console.log('User state:', user);
+
+      if (!user) {
+        return;
+      } else {
+        this.user.name = user.displayName;
+        this.user.email = user.email;
+        this.user.uid = user.uid;
+
+        console.log('User data => ', this.user);
+      }
+    });
+  }
+
+  login(provider: string) {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.user = {};
+    this.afAuth.auth.signOut();
+  }
 
   loadMessages() {
     this.itemsCollection = this.afs.collection<Message>('chats', ref => ref.orderBy('date', 'desc').limit(7));
